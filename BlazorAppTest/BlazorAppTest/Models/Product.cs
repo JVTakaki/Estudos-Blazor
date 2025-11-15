1a612267c4ea563c533e4e48bbc21f71bcb02aa4
GeovaniPrestes <geovani@sistemasbr.net>
2025-11-15 16:42:14 -0300

    criando um projeto Blazor do zero novamente para poder ter a base melhor estruturada para quando eu for fazer a aplicação hibrida

---
+++ BlazorAppTest/BlazorAppTest/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppTest.Models
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<ProductProp> ProductProp { get; set; }    
+    }
+}
--- ProjetoBlazorTeste/ProjetoBlazorTeste/Services/IPersonService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorInWinForms.Services;
-
-internal interface IPersonService
-{
-    Person GetPerson(int personId);
-}
-
-public record Person(int PersonId, string FirstName, string LastName, string Role);
